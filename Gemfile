f274ee4e3548a28bc12aadd17786bb80a39821f2
didil <haritahadil@gmail.com>
2013-10-24 10:22:16 -0500

    getting started

--- Gemfile
+++ Gemfile
@@ -1,12 +1,7 @@
 source 'https://rubygems.org'
 ruby '1.9.3'
 gem 'rails', '3.2.13'
-gem 'sqlite3'
-group :assets do
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '>= 1.0.3'
-end
+
 gem 'jquery-rails'
 gem 'bootstrap-sass', '~> 2.3.2.2'
 gem 'cancan'
@@ -15,25 +10,49 @@ gem 'figaro'
 gem 'haml-rails'
 gem 'rolify'
 gem 'simple_form'
+gem "awesome_print"
+gem 'capistrano'
+gem 'whenever', :require => false
+gem 'sidekiq', ">= 2.15.0"
+gem 'sidekiq-client-cli', ">= 0.1.3"
+gem 'airbrake'
+gem 'newrelic_rpm'
+
 group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
   gem 'therubyracer', :platform=>:ruby
 end
+
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
   gem 'html2haml'
   gem 'quiet_assets'
   gem 'rails_layout'
+  gem "webrick"
+  gem "bullet"
 end
+
 group :development, :test do
   gem 'factory_girl_rails'
   gem 'rspec-rails'
 end
-group :production do
-  gem 'unicorn'
+
+group :development , :production do
+  gem "pg"
 end
+
+group :production do
+  gem "unicorn"
+end
+
 group :test do
   gem 'capybara'
+  gem "capybara-webkit"
   gem 'database_cleaner', '1.0.1'
   gem 'email_spec'
+  gem 'sqlite3'
+  gem "zeus"
+  gem "timecop"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'email_spec'
-require 'rspec/autorun'
+require 'database_cleaner'
+require 'capybara-webkit'
+require 'sidekiq/testing'
+
+Capybara.javascript_driver = :webkit
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -23,11 +27,6 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
-
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
@@ -38,14 +37,27 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  
+
+  config.use_transactional_fixtures = false
+
   config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
   end
+
   config.before(:each) do
     DatabaseCleaner.start
   end
+
   config.after(:each) do
     DatabaseCleaner.clean
   end
+
 end
